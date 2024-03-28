106e1d40022edae93fe68346e510f25cc05b3515
Pavel Kratochvil <xkrato61@vutbr.cz>
2024-03-28 14:52:10 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,125 @@
+import os
+import random
+import subprocess
+
+EXECUTABLE='../gol'
+
+N_TESTS = 3     #number of test cases
+MAX_LENGTH = 10 #width of the field
+MAX_COUNT = 10  #height of the field
+MIN_PROC = 0    #minimum number of processes
+MAX_PROC = 10   #maximum number of processes
+MIN_IT = 0      #minimum number of iterations
+MAX_IT = 15     #maximum number of iterations
+
+def read_field(filename):
+    with open(filename, 'r') as f:
+        field = []
+        for row in f.read().splitlines():
+            field.append([int(cell) for cell in row.strip()])
+    return field
+
+def count_neighbors(field, x, y):
+    neighbors = 0
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < len(field) and 0 <= ny < len(field[0]):
+                neighbors += field[nx][ny]
+    return neighbors
+
+def next_state(current_field):
+    next_field = []
+    neighbors_field = []
+    for x in range(len(current_field)):
+        next_row = []
+        for y in range(len(current_field[0])):
+            neighbors = count_neighbors(current_field, x, y)
+            neighbors_field.append(neighbors)
+            if current_field[x][y] == 1 and neighbors in [2, 3]:
+                next_row.append(1)
+            elif current_field[x][y] == 0 and neighbors == 3:
+                next_row.append(1)
+            else:
+                next_row.append(0)
+        next_field.append(next_row)
+    return next_field
+
+def print_field(field):
+    for row in field:
+        print(''.join(map(str, row)))
+
+def iterate(field, n_iters):
+    f = field
+    for i in range(n_iters):
+        f = next_state(f)
+    return f
+
+def save_test_case(idx, testcase, name="input"):
+    directory = f"tests/case_{idx}/"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open(f"{directory}/{name}.txt", 'w') as f:
+        for line in testcase:
+            f.write(''.join(str(x) for x in line))
+            f.write('\n')
+
+def generate(n, max_length=MAX_LENGTH, max_count=MAX_COUNT):
+    tests = []
+    for i in range(n):
+        test_case = []
+        # only even numbers
+        length = random.randint(1, max_length // 2) * 2
+        count = random.randint(1, max_count // 2) * 2
+
+        for c in range(count):
+            line = []
+            for l in range(length):
+                line.append(random.choice((0, 1)))
+            test_case.append(line)
+        tests.append(test_case)
+
+    return tests
+            
+def remove_tests(directory):
+    if os.path.exists(directory):
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                remove_tests(file_path)
+        os.rmdir(directory)
+
+def launch_executable(executable_path, np, it, case_idx):
+    with open(f'./tests/case_{case_idx}/case_{case_idx}_np_{np}_it_{it}.out', 'w') as out_file, open(f'./tests/case_{case_idx}/case_{case_idx}_np_{np}_it_{it}.err', 'w') as err_file:
+        subprocess.run(['mpirun', '--oversubscribe', '-np', str(np), executable_path, f'./tests/case_{case_idx}/input.txt', str(it)], stdout=out_file, stderr=err_file)
+
+def compare_results(idx, np, it):
+    with open(f'./tests/case_{idx}/differences', 'a') as diff_file:
+        reference_path = f'./tests/case_{idx}/i_{it}.txt'
+        test_path = f'./tests/case_{idx}/case_{idx}_np_{np}_it_{it}.out'
+        result = subprocess.run(['diff', reference_path, test_path], stdout=diff_file)
+        if result.returncode != 0:
+            diff_file.write(f'test case idx:{idx} np:{np} it:{it}\n')
+            print(f'Test case idx:{idx} np:{np} it:{it} failed')
+
+if __name__ == '__main__':
+    tests = generate(N_TESTS)
+
+    remove_tests('./tests')
+
+    for i in range(N_TESTS):
+        save_test_case(i, tests[i])
+        for l in range(MIN_IT, MAX_IT + 1):
+            save_test_case(i, iterate(read_field(f'./tests/case_{i}/input.txt'), l), f'i_{l}')
+        
+        # test on multiple number of processes
+        for np in range(MIN_PROC, MAX_PROC + 1):
+            # test multiple iterations forward
+            for l in range(MIN_IT, MAX_IT + 1):
+                    launch_executable(EXECUTABLE, np, l, i)
+                    compare_results(i, np, l)
---
+++ readme.md
@@ -0,0 +1,61 @@
+## TESTY NA 2. PRL PROJEKT - GAME OF LIFE
+
+#### Priebeh testu
+1. Vygenerovanie testcase-ov podla parametrov v `main.py`
+2. Spustia sa vsetky mozne prednastavene konfiguracie parametrov:
+   - `np` - pocet procesov s ktorym sa spusta binarka
+   - `it` - pocet iteracii od inicialneho stavu
+
+- Pri neuspesnom teste (referencny vystup `i_{idx}.txt` != `case_{test_id}_np_{n_processes}_it_{iterations}.out`) sa rozdiel suborov ulozi do `case_{idx}/difference` a python skript na to upozorni na stdout za behu.
+
+
+- Vsetky medzivysledky (stdout/stderr z binarky) sa ukladaju do zlozky daneho test case-u pod nazvom: `case_{test_id}/case_{test_id}_np_{n_processes}_it_{iterations}.out/.err` kde:
+    - `test-id`     - id test case-u
+    - `n_processes` - pocet procesov pri spusteni
+    - `iterations`  - pocet iteracii pri spusteni
+
+  Takze k neuspesnym testom sa da vratit a skusit ich spusti manualne, pripadne porovnat vysledky rucne atd.
+
+#### ❗ Requirements
+* Python 3
+
+#### Pouzitie
+1. Naklonujte repozitar do zlozky s game of life binarkou alebo zmente `EXECUTABLE` v `main.py`
+2. Spustite `python3 main.py`
+
+#### ⚠️ Disclaimer
+* Neviem ako budu testy fungovat na wine, ked tak niekto poslite PR s fixom
+
+* Testy su IBA orientacne.
+
+#### Struktura generovanych testovacich suborob
+Skript generuje subory `input.txt` a nasledne stav po kazdej iteracii pre zadany pocet iteracii. Na konci kazdeho zo suborov je newline.
+
+Vygeneruje sa struktura suborov:
+
+* zlozka `./tests/`
+
+  * `case_{id}/` podzlozka pre jednotlive test cases
+
+    * `input.txt` inicialny stav pola:
+        ```
+        001000
+        011001
+        001100
+        010110
+        100001
+        001010
+        110111
+        011001
+        ```
+    *  `i_{it}.txt` referencny stav pola po `it` pocte iteracii (napr. `i_1.txt`):
+        ```
+        011000
+        010000
+        000000
+        010110
+        011001
+        101000
+        100001
+        111101
+        ```
